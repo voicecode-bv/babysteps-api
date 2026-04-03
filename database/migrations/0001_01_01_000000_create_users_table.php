9c2e7970ca01333d21fd9fb41c01c31efca06aed
Michael Blijleven <info@voicecode.nl>
2026-04-03 20:31:27 +0200

    WIP

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,6 +14,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username')->unique();
+            $table->string('avatar')->nullable();
+            $table->text('bio')->nullable();
+            $table->string('locale', 5)->default('en');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
