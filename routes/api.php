a6b316643d7dbbaf5f0399c2ec4fae305bf4d268
Michael Blijleven <info@voicecode.nl>
2026-04-03 20:51:56 +0200

    Add LikeController implementation and API route definitions

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,28 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CommentController;
+use App\Http\Controllers\Api\FeedController;
+use App\Http\Controllers\Api\LikeController;
+use App\Http\Controllers\Api\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/auth/login', [AuthController::class, 'login'])->middleware('throttle:5,1');
+Route::post('/auth/register', [AuthController::class, 'register']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/auth/me', [AuthController::class, 'me']);
+    Route::post('/auth/logout', [AuthController::class, 'logout']);
+
+    Route::get('/feed', FeedController::class);
+
+    Route::get('/posts/{post}', [PostController::class, 'show']);
+    Route::post('/posts', [PostController::class, 'store']);
+    Route::delete('/posts/{post}', [PostController::class, 'destroy']);
+
+    Route::post('/posts/{post}/comments', [CommentController::class, 'store']);
+    Route::delete('/comments/{comment}', [CommentController::class, 'destroy']);
+
+    Route::post('/posts/{post}/like', [LikeController::class, 'store']);
+    Route::delete('/posts/{post}/like', [LikeController::class, 'destroy']);
+});
